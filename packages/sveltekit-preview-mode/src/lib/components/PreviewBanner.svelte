cd3c1a939c82caedbd6afbceddea4ef1d1ea9b7f
Bert B <44912991+bertybot@users.noreply.github.com>
2023-09-06 14:54:00 -0400

    Update PreviewBanner.svelte

--- packages/sveltekit-preview-mode/src/lib/components/PreviewBanner.svelte
+++ packages/sveltekit-preview-mode/src/lib/components/PreviewBanner.svelte
@@ -4,10 +4,11 @@
 
   let exit_link: string;
 
-  $: {
-    $page.url.searchParams.set($page.data.exitPreviewQueryParam, "true");
-    exit_link = $page.url.toString();
-  }
+	$: if (isPreview()) {
+		const url = new URL($page.url);
+		url.searchParams.set($page.data.exitPreviewQueryParam, 'true');
+		exit_link = url.toString();
+	}
 </script>
 
 {#if isPreview()}
